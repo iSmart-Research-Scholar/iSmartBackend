548dec3390fe6c8c80524b5d4a1d0d704325f453
Satyam Gupta <202051169@iiitvadodara.ac.in>
2022-10-06 09:12:58 +0530

    defined searcher logic

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
+from turtle import title
 from urllib import response
 from django.shortcuts import render,HttpResponse
-
+from scripts.searcher import searcher 
+from django.http import JsonResponse
 
 # Create your views here.
-
+se=searcher()
 
 def home(request):    
     return render(request,'index.html')
@@ -11,7 +13,10 @@ def home(request):
 def search(request):
     if request.method=='POST':
         query=request.POST.get('search_query')
-        return HttpResponse(query)
+        title=request.POST.get('title')
+        author=request.POST.get('author')
+        result=se.searchIEEE(query,title,author)
+        return JsonResponse(result)
 
 
 
