0ce598942efe7357dfe4b4350ae5f1f2985c7d3f
Nivas <vsnsainivasand2003@gmail.com>
2022-10-15 11:28:03 +0530

    Ranking modified

--- scripts/ranking.py
+++ scripts/ranking.py
@@ -13,7 +13,6 @@ def ranking(data, query, choosen):
         listWords[index] = word
     listStore = []
     dictScore = {}
-    # Calculating the IDF for each word
     dictIDFTitle = {}
     dictIDFWord = {}
     for i in range(len(articles)):
@@ -37,7 +36,7 @@ def ranking(data, query, choosen):
                     else:
                         dictIDFWord[word] += 1
     
-
+    citationsWeight = 2
     for i in range(len(articles)):
         weight = 0
         title = articles[i]['title']
@@ -46,10 +45,16 @@ def ranking(data, query, choosen):
         citations = articles[i]['citing_paper_count']
         title = ps.stem(remove_stopwords(title).lower())
         abstract = ps.stem(remove_stopwords(abstract).lower())
+        countTitle = 0
+        countAbstract = 0
         for word in listWords:
-            countTitle = title.count(word)
+            for titleWord in title:
+                if titleWord.find(word) != -1:
+                    countTitle += 1
             if 'abstract' in articles[i]:
-                countAbstract = abstract.count(word)
+                for abstractWord in abstract:
+                    if abstractWord.find(word) != -1:
+                        countAbstract += 1
             totalTitle = len(title.split(' '))
             if 'abstract' in articles[i]:
                 totalAbstract = len(abstract.split(' '))
@@ -57,8 +62,36 @@ def ranking(data, query, choosen):
                 weight += (totalRetrieved/(dictIDFTitle[word]+1)) * (countTitle/totalTitle)
             if word in dictIDFWord and 'abstract' in articles[i]:
                 weight += (totalRetrieved/(dictIDFWord[word]+1)) * (countAbstract/totalAbstract)
+
         
-        weight = weight + citations*0.5
+        if 'index_terms' in articles[i] and 'ieee_terms' in articles[i]['index_terms']:
+            indexTermsWeight = 3
+            presentIndex = 0
+            totalIndex = 0 
+            listIndexTerms = articles[i]['index_terms']['ieee_terms']['terms']
+            for term in listIndexTerms:
+                term = remove_stopwords(term).lower()
+                for wordTerm in term:
+                    if ps.stem(wordTerm) in listWords:
+                        presentIndex += 1
+                    totalIndex += 1
+            weight = weight + (presentIndex/totalIndex)*indexTermsWeight
+
+
+        if 'index_terms' in articles[i] and 'author_tems' in articles[i]['index_terms']:
+            authorTermsWeight = 2
+            presentAuthor = 0
+            totalAuthor = 0
+            listAuthorTerms = articles[i]['index_terms']['author_terms']['terms']
+            for term in listAuthorTerms:
+                term = remove_stopwords(term).lower()
+                for wordTerm in term:
+                    if ps.stem(wordTerm) in listWords:
+                        presentAuthor += 1
+                    totalAuthor += 1
+            weight = weight + (presentAuthor/totalAuthor)*authorTermsWeight
+            
+        weight = weight + citations*citationsWeight
         weight = round(weight, 2)
         listStore.append([weight, articles[i]['publication_year'], articles[i]['citing_paper_count'], i])
     if choosen == 1:
