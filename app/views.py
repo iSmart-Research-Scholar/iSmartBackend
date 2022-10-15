faba6600cccd5471c41f1e28237a1f73f552401f
Abhay Jagtap <abhay.jagtap000@gmail.com>
2022-10-15 15:38:01 +0530

    Made Heroku Compatible

--- app/views.py
+++ app/views.py
@@ -4,18 +4,20 @@ from django.shortcuts import render,HttpResponse
 from scripts.searcher import searcher 
 from django.http import JsonResponse
 from scripts import ranking
+import requests,environ,re
+
+se = searcher()
 
 # Create your views here.
-se=searcher()
 
 def home(request):    
     return render(request,'index.html')
 
 def search(request):
-    if request.method=='POST':
-        query=request.POST.get('search_query')
-        title=request.POST.get('title')
-        author=request.POST.get('author')
+    if request.method=='GET':
+        query=request.GET.get('search_query')
+        title=request.GET.get('title')
+        author=request.GET.get('author')
         result=se.searchIEEE(query,title,author)
         return JsonResponse(result, safe=False)
 
--- scripts/searcher.py
+++ scripts/searcher.py
@@ -8,7 +8,7 @@ env=environ.Env(
 )
 environ.Env.read_env("D:/Hackathons/Backend/iSmartBackend/.env")
 class searcher:
-    def __urlify(self,s):
+    def _urlify(self,s):
     
         # Remove all non-word characters (everything except numbers and letters)
         s = re.sub(r"[^\w\s]", '', s)
@@ -21,9 +21,9 @@ class searcher:
     def searchIEEE(self,query='',title='',author=''):
         apiKey=env('API_KEY')
         querytosend = query
-        title=self.__urlify(title)
-        author=self.__urlify(author)
-        query=self.__urlify(query)
+        title=self._urlify(title)
+        author=self._urlify(author)
+        query=self._urlify(query)
         link=f'http://ieeexploreapi.ieee.org/api/v1/search/articles?apikey={apiKey}&format=json&max_records=1000&start_record=1&sort_order=asc&sort_field=article_number&querytext={query}&article_title={title}&author={author}'
         result=requests.get(link)
         jsonobj = ranking.ranking(result.json(), querytosend, 00)
